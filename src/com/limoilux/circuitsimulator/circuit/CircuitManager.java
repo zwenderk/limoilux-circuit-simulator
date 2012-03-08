82922f6cdc155ddde7debf140311dc95b31a8ab4
dbernard456 <dbernard456@gmail.com>
2012-03-08 23:31:36 +0000

    transfert de registe vers DumpManager

--- src/com/limoilux/circuitsimulator/circuit/CircuitManager.java
+++ src/com/limoilux/circuitsimulator/circuit/CircuitManager.java
@@ -4,7 +4,6 @@ package com.limoilux.circuitsimulator.circuit;
 import java.awt.Color;
 import java.awt.Graphics;
 
-import com.limoilux.circuitsimulator.io.DumpManager;
 
 public class CircuitManager
 {
